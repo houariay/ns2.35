0a021f1c42948b67da9dac7c5108350adaa072c5
houariay <161453401+houariay@users.noreply.github.com>
2024-02-27 11:22:46 +0100

    Update aodv.h

--- aodv/aodv.h
+++ aodv/aodv.h
@@ -35,7 +35,7 @@ The AODV code developed by the CMU/MONARCH group was optimized and tuned by Sami
 //#include <packet.h>
 //#include <sys/types.h>
 //#include <cmu/list.h>
-//#include <scheduler.h>
+#include <scheduler.h>
 
 #include <cmu-trace.h>
 #include <priqueue.h>
@@ -121,6 +121,75 @@ class AODV;
 #define BAD_LINK_LIFETIME       3               // 3000 ms
 #define MaxHelloInterval        (1.25 * HELLO_INTERVAL)
 #define MinHelloInterval        (0.75 * HELLO_INTERVAL)
+#ifndef timer_handler_h
+#define timer_handler_h
+
+#include "scheduler.h"
+
+/*
+ * Abstract base class to deal with timer-style handlers.
+ *
+ *
+ * To define a new timer, subclass this function and define handle:
+ *
+ * class MyTimer : public TimerHandler {
+ * public:
+ *	MyTimer(MyAgentClass *a) : AgentTimerHandler() { a_ = a; }
+ *	virtual double expire(Event *e);
+ * protected:
+ *	MyAgentClass *a_;
+ * };
+ *
+ * Then define expire.
+ *
+ * Often MyTimer will be a friend of MyAgentClass,
+ * or expire() will only call a function of MyAgentClass.
+ *
+ * See tcp-rbp.{cc,h} for a real example.
+ */
+#define TIMER_HANDLED -1.0	// xxx: should be const double in class?
+
+class TimerHandler : public Handler {
+public:
+	TimerHandler() : status_(TIMER_IDLE) { }
+
+	void sched(double delay);	// cannot be pending
+	void resched(double delay);	// may or may not be pending
+					// if you don't know the pending status
+					// call resched()
+	void cancel();			// must be pending
+	inline void force_cancel() {	// cancel!
+		if (status_ == TIMER_PENDING) {
+			_cancel();
+			status_ = TIMER_IDLE;
+		}
+	}
+	enum TimerStatus { TIMER_IDLE, TIMER_PENDING, TIMER_HANDLING };
+	int status() { return status_; };
+
+protected:
+	virtual void expire(Event *) = 0;  // must be filled in by client
+	// Should call resched() if it wants to reschedule the interface.
+
+	virtual void handle(Event *);
+	int status_;
+	Event event_;
+
+private:
+	inline void _sched(double delay) {
+		(void)Scheduler::instance().schedule(this, &event_, delay);
+	}
+	inline void _cancel() {
+		(void)Scheduler::instance().cancel(&event_);
+		// no need to free event_ since it's statically allocated
+	}
+};
+
+// Local Variables:
+// mode:c++
+// End:
+
+#endif /* timer_handler_h */
 
 /*
   Timers (Broadcast ID, Hello, Neighbor Cache, Route Cache)
